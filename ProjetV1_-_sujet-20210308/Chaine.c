edf2b00943db690061a9c1958dfef7f5b5262543
LucieChenz <62623172+LucieChenz@users.noreply.github.com>
2021-03-20 16:43:07 +0100

    Update Chaine.c
    做了 exo1.1 和 exo1.5
    加了 fonction libererCellChaine(CellChaine*); 和 libererChaines(Chaines*);

--- ProjetV1_-_sujet-20210308/Chaine.c
+++ ProjetV1_-_sujet-20210308/Chaine.c
@@ -1,8 +1,47 @@
 #include "Chaine.h"
 #include <stdio.h>
 
+void libererCellChaine(CellChaine * cc){
+  if(!cc){
+    return;
+  }
+  
+  CellPoint * cpCour = cc -> points;
+  CellPoint * cpPrec;
+
+  while(cpCour){
+    cpPrec = cpCour;
+    cpCour = cpCour -> suiv;
+    free(cpPrec);
+  }
+  
+  free(cc);
+  return;
+}
+
+void libererChaines(Chaines* C){
+  if(!C){
+    return;
+  }
+  CellChaine * ccCour = C -> chaines;
+  CellChaine * ccPrec;
+
+  while(ccCour){
+    ccPrec = ccCour;
+    ccCour = ccCour -> suiv;
+    free(ccPrec);
+  }
+  
+  free(C);
+  return;
+}
+
+
+
+
+
 //Q1.1
-Chaines* lectureChaines(FILE *f)
+Chaines* lectureChaines(FILE * f)
 {  
   char buf[256];
   int x, y; //lecture CellPoint
@@ -12,7 +51,10 @@ Chaines* lectureChaines(FILE *f)
   
   //Création ensemble de chaînes 
   Chaines *C = (Chaines *) malloc(sizeof(Chaines));
-  
+  if(!C){
+    printf("Erreur lors de l'allocation Chaines.\n");
+    return NULL;
+  }
   fgets(buf, 256, f);
   buf[strlen(buf) - 1] = '\0';
   sscanf(buf, "NbChain: %d", &nbChaines);
@@ -31,6 +73,11 @@ Chaines* lectureChaines(FILE *f)
     
     //Création liste de chaînes
     CellChaine* cc = (CellChaine*)malloc(sizeof(CellChaine));
+    if(!cc){
+      printf("Erreur lors de l'allocation CellChaine.\n");
+      libererChaines(C);
+      return NULL;
+    }
     
     fgets(buf, 256, f);
     buf[strlen(buf) - 1] = '\0';
@@ -45,6 +92,12 @@ Chaines* lectureChaines(FILE *f)
        
       //Création de points
       CellPoint* cp = (CellPoint*)malloc(sizeof(CellPoint));
+      if(!cp){
+        printf("Erreur lors de l'allocation CellPoint.\n");
+        libererCellChaine(cc);
+        libererChaines(C);
+        return NULL;
+      }
       cp -> x = x;
       cp -> y = y;
       
@@ -64,4 +117,30 @@ Chaines* lectureChaines(FILE *f)
 void ecrireChaines(Chaines *C, FILE *f)
 void afficheChainesSVG(Chaines *C, char* nomInstance)
 double longueurTotale(Chaines *C)
-int comptePointsTotal(Chaines *C)
+  
+  
+int comptePointsTotal(Chaines *C){
+  int nbPointsTotal = 0;
+  CellChaine* cc = (CellChaine*)malloc(sizeof(CellChaine));
+  if(!cc){
+    printf("Erreur lors de l'allocation CellChaine.\n");
+    return -1;
+  }
+  CellPoint* cp = (CellPoint*)malloc(sizeof(CellPoint));
+  if(!cp){
+    printf("Erreur lors de l'allocation CellPoint.\n");
+    libererCellChaine(cc);
+    return -1;
+  }
+  
+  cc = C -> chaines;
+  while(cc){
+    cp = cc -> points;
+    while(cp){ 
+      nbPointsTotal += 1;
+      cp = cp -> suiv;
+    } 
+    cc = cc -> suiv;
+  }
+    
+}
