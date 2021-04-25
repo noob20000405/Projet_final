3304b8e48eb89dc6510f09424ccbcea804982ca3
noob20000405 <vincent4869code@gmail.com>
2021-04-25 14:58:48 +0200

    Add files via upload

--- exo4/Hachage.c
+++ exo4/Hachage.c
@@ -3,6 +3,7 @@
 #include "Hachage.h"
 
 
+/* Q 4.2 */
 void libereTableHachage(TableHachage * H) {
     if (!H) return ;
     
@@ -23,17 +24,20 @@ void libereTableHachage(TableHachage * H) {
 }
 
 double fonctionClef(double x, double y){
-    return y + (x + y) * (x + y + 1) / 2.0;
+    return y + (x + y) * (x + y + 1) * 1.0 / 2;
 }
 
 
+/* Q 4.3 */
 int fonctionHachage(double k, int m) {
   double A = (sqrt(5) - 1) / 2;
-  return ((int)(m * (k * A - ((int)(k * A)))));
+  return ((int)(m * (k * A - ((long int)(k * A)))));/////////////////
 }
 
 
+/* Q 4.4 */
 Noeud* rechercheCreeNoeudHachage(Reseau* R, TableHachage* H, double x, double y){
+
     double clef = fonctionClef(x,y);
     int index = fonctionHachage(clef, H -> m);
     CellNoeud * CNoeudH = (H -> tabN)[index];
@@ -41,48 +45,49 @@ Noeud* rechercheCreeNoeudHachage(Reseau* R, TableHachage* H, double x, double y)
 
     //Nous comparons les coordonnes de NoeudH dans la liste CNoeudH avec x et y passés en paramètres
     while(CNoeudH != NULL){
-    
         NoeudH = CNoeudH -> nd;
-        if((NoeudH -> x == x) && (NoeudH -> y == y)){
-            return NoeudH;
-        }
+
+        if((NoeudH -> x) == x && (NoeudH -> y) == y) return NoeudH;
         CNoeudH = CNoeudH -> suiv;
     }
-    
+
     //Dans le cas où le noeuds de coordonnees (x,y) n'est pas dans la table, nous la rajoutons dans la table de hachage et dans la liste
     Noeud * NoeudNouv = (Noeud *)malloc(sizeof(Noeud));
     NoeudNouv -> num = ((R -> nbNoeuds) + 1);
     NoeudNouv -> x = x;
     NoeudNouv -> y = y;
     NoeudNouv -> voisins = NULL;
-    
-    /* Dans la liste */
+
+    /* L'ajouter dans la liste */
     CellNoeud * CNouvL = (CellNoeud*)malloc(sizeof(CellNoeud));
     CNouvL -> nd = NoeudNouv;
     CNouvL -> suiv = R -> noeuds;
     R -> noeuds = CNouvL;
     R -> nbNoeuds++;
-    
-    /* Dans la table de hachage */
+
+    /* L'ajouter dans la table de hachage */
     CellNoeud * CNouvH = (CellNoeud*)malloc(sizeof(CellNoeud));
     CNouvH -> nd = NoeudNouv;
     CNouvH -> suiv = (H -> tabN)[index];
-    (H -> tabN)[index] = CNouvH;///////////
+    (H -> tabN)[index] = CNouvH;
     H -> nE++;
-    
+
     return NoeudNouv;
     
 }
 
 
+/* Q 4.5 */
 Reseau* reconstitueReseauHachage(Chaines *C, int M){
     if(!C) return NULL;
 
-
+    /* Declarer et initialiser la table hachage et le reseau */
     TableHachage * H = (TableHachage *)malloc(sizeof(TableHachage));
     H -> nE = 0;
     H -> m = M;
     H -> tabN = (CellNoeud**)malloc(M * sizeof(CellNoeud*));
+
+    for (int i = 0 ; i < M ; i++) (H -> tabN[i]) = NULL;
     
     Reseau * r = (Reseau *)malloc(sizeof(Reseau));
     r -> gamma = C -> gamma;
--- exo4/Hachage.h
+++ exo4/Hachage.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include "Reseau.h"
 
+/* Q 4.1 */
 typedef struct tableHachage {
     int nE; //nombre d'éléments contenus dans la table de hachage
     int m; //taille de la table de hachage
@@ -12,11 +13,13 @@ typedef struct tableHachage {
 
 void libereTableHachage(TableHachage * H);
 
+/* Q 4.2 */
 double fonctionClef(double x, double y);
+/* Q 4.3 */
 int fonctionHachage(double k, int m);
-
+/* Q 4.4 */
 Noeud* rechercheCreeNoeudHachage(Reseau* R, TableHachage* H, double x, double y);
-
+/* Q 4.5 */
 Reseau* reconstitueReseauHachage(Chaines *C, int M);
 
 #endif /* Hachage_h */
