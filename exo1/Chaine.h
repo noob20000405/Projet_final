e1e279a9e75d6ccf8e93bce6cb6788543c1689b9
LucieChenz <62623172+LucieChenz@users.noreply.github.com>
2021-04-26 12:22:58 +0200

    Update Chaine.h
    Commentaires : description de fonctions

--- exo1/Chaine.h
+++ exo1/Chaine.h
@@ -24,15 +24,22 @@ typedef struct {
 void libererCellChaine(CellChaine* cc);
 void libererChaines(Chaines* C);
 
-/* Q 1.1 */
+/* Q 1.1 *///Allouer, remplir et retourner une instance de notre structure à partir d’un fichier
 Chaines* lectureChaines(FILE *f);
-/* Q 1.2 */
+
+/* Q 1.2 *///Ecrire dans un fichier le contenu de Chaines
 void ecrireChaines(Chaines *C, FILE *f);
-/* Q 1.3 */
+
+/* Q 1.3 *///Créer le fichier SVG en html à partir d’un struct Chaines
 void afficheChainesSVG(Chaines *C, char* nomInstance);
+
 /* Q 1.4 */
+//Calcule la longueur physique d’une chaîne
+double longueurChaine(CellChaine * c);
+//Calcule la longueur physique totale des chaînes
 double longueurTotale(Chaines *C);
-/* Q 1.5 */
+
+/* Q 1.5 *///Donne le nombre total d’occurrences de points
 int comptePointsTotal(Chaines *C);
 
 /* Q 6.2 */
