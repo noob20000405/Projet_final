f2354b7b1dc0f175e76db095504f38ff6ac0f803
noob20000405 <vincent4869code@gmail.com>
2021-04-26 13:56:15 +0200

    Add files via upload

---
+++ exo1/ChaineMain.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "Chaine.h"
+
+/* Q 1.2 */
+int main(int argc, char * argv[]) {
+    if (argc != 2) {
+        printf("Nombre de parametres incorrect\n");
+        return 1;
+    }
+    
+    FILE * fIn = fopen(argv[1], "r");
+    FILE * fOut = fopen("ecrire_chaines.txt", "w");
+    Chaines * c = lectureChaines(fIn);
+    ecrireChaines(c, fOut);
+    
+    printf("longueurTotale : %lf\n", longueurTotale(c));
+    printf("comptePointsTotal : %d\n", comptePointsTotal(c));
+    
+    libererChaines(c);
+    fclose(fIn);
+    fclose(fOut);
+    
+    return 0;
+}
